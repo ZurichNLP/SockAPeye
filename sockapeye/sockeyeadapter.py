ba8306d2ff7c0a7fc154d69c218f686f71269025
Mathias Müller <mathias.mueller@uzh.ch>
2019-09-23 18:04:25 +0200

    preprocessor steps

--- sockapeye/sockeyeadapter.py
+++ sockapeye/sockeyeadapter.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import sockeye.inference as inference
 
 from sockeye.utils import determine_context
@@ -15,8 +17,8 @@ class SockeyeAdapter:
         self.models, self.source_vocabs, self.target_vocab = inference.load_models(
             context=context,
             max_input_len=None,
-            beam_size=10,
-            batch_size=100,
+            beam_size=5,
+            batch_size=1,
             model_folders=[model_path],
             checkpoints=None,
             softmax_temperature=None,
@@ -50,6 +52,6 @@ class SockeyeAdapter:
     # Arguments: Namespace(avoid_list=None, batch_size=100, beam_prune=0, beam_search_stop='all', beam_size=10, brevity_penalty_constant_length_ratio=0.0, brevity_penalty_type='none', brevity_penalty_weight=1.0, bucket_width=10, checkpoints=None, chunk_size=None, config=None, device_ids=[-1], disable_device_locking=False, ensemble_mode='linear', input=None, input_factors=None, json_input=False, length_penalty_alpha=1.0, length_penalty_beta=0.0, lock_dir='/tmp', loglevel='INFO', max_input_len=None, max_output_length_num_stds=2, models=['/Users/raphael/projects/sockeye-toy-models/mt19_u6_model/models/model_wmt17'], nbest_size=1, output=None, output_type='translation', override_dtype=None, quiet=False, restrict_lexicon=None, restrict_lexicon_topk=None, sample=None, seed=None, skip_topk=False, softmax_temperature=None, strip_unknown_words=False, sure_align_threshold=0.9, use_cpu=True)
 
     def translate(self, input):
-        inputs = inference.make_input_from_plain_string(1, input)
+        inputs = inference.make_input_from_plain_string(0, input)
         outputs = self.translator.translate([inputs])
         return outputs[0].translation
--- sockapeye/translate.py
+++ sockapeye/translate.py
@@ -1,9 +1,7 @@
-from translator.preprocessing.normalizer import Normalizer
-from translator.preprocessing.tokenizer import Tokenizer, Detokenizer
-from translator.preprocessing.truecaser import Truecaser, Detruecaser
-from translator.preprocessing.bpe import BytePairEncoderSegment, bpe_decode_segment
-from translator.preprocessing.external import ExternalProcessor
-from translator.sockeyeadapter import SockeyeAdapter
+#! /usr/bin/env python3
+
+from . import processor
+from . import adapter
 
 class Translator:
     def __init__(self):
@@ -11,21 +9,35 @@ class Translator:
         self.src_lang = "de"
         self.trg_lang = "en"
         self.model_base = "/Users/raphael/projects/sockeye-toy-models/mt19_u6_model"
-        self.normalizer = Normalizer(self.src_lang)
-        self.tokenizer = Tokenizer(self.src_lang)
-        self.detokenizer = Detokenizer(self.trg_lang)
-
         truecase_model = self.model_base + "/shared_models/truecase_model." + self.src_lang
-        self.truecaser = Truecaser(truecase_model)
-        self.detruecaser = Detruecaser()
 
         bpe_model = self.model_base + "/shared_models/" + self.src_lang + self.trg_lang + ".bpe"
         bpe_vocab = self.model_base + "/shared_models/vocab." + self.src_lang
-        self.bpe = BytePairEncoderSegment(bpe_model, bpe_vocab)
+        vocab_threshold = 50
+
+        preprocessor_steps = [processor.NormalizeStep(lang=self.src_lang),
+                              processor.TokenizeStep(lang=self.src_lang),
+                              processor.TruecaseStep(lang=self.src_lang, load_from=truecase_model),
+                              processor.BpeStep(lang=self.src_lang,
+                                                load_from=bpe_model,
+                                                vocab=bpe_vocab,
+                                                vocab_threshold=vocab_threshold)]
+
+        self.preprocessor = processor.Processor(steps=preprocessor_steps)
+
+
 
         self.sockeye = SockeyeAdapter(self.model_base + "/models/model_wmt17")
 
-    def translate_lines(self, input_lines):
+    def translate_lines(self, lines):
+
+        for line in lines:
+            line = self.preprocessor.process(line)
+            line = self.translator.translate(line)
+            line = self.postprocessor.process(line)
+
+            yield line
+
         lines = map(self.preprocess, input_lines)
         
         lines = map(self.translate, lines)
